147308ad11362b383c6da4d3ddd98b94d5905d77
uidp2242 <uidp2242@KFL5BX0W.cw01.contiwan.com>
2017-09-12 14:37:05 +0200

    fertig für merge

--- src/xml/XML_translator.java
+++ src/xml/XML_translator.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.rmi.RemoteException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
@@ -30,15 +31,26 @@ import model.Task;
 import model.User;
 import xml.projectlist.Projectlist;
 
-
+/*
+ * Übersetzungklasse
+ * XML to Projekt and back
+ * 
+ * 
+ * 
+ */
 public class XML_translator  
 {
 	
-	public XML_translator()
-	{
-		
-	}
 	
+	/**
+	 * Methode erhält das Projekt, übersetz es für die XML Project und XML Projectlist und gibt es weiter an den XML Server
+	 * @param orginalProjekt
+	 * @throws JAXBException
+	 * @throws FileNotFoundException
+	 * @throws DatatypeConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
 	public void createProject(Projekt orginalProjekt) throws JAXBException, FileNotFoundException, DatatypeConfigurationException, SAXException, IOException
 	{
 		ObjectFactory ocFac = new ObjectFactory();
@@ -47,7 +59,7 @@ public class XML_translator
 		
 		ProjectOverviewEntries.UserEntries  usList = ocFac.createProjectlistProjectOverviewEntriesUserEntries();
 		
-		for (User us:users.values()) 
+		for (User us:users.values())   // übergibt User
 		{
 			ProjectOverviewEntries.UserEntries xmlUser = ocFac.createProjectlistProjectOverviewEntriesUserEntries();
 			xmlUser.setValue(us.getNutzername());
@@ -62,7 +74,7 @@ public class XML_translator
 		HashMap<String, model.Task> tasks = new HashMap<String, model.Task>();
 		tasks = orginalProjekt.getTasks();
 		
-		for(model.Task ta:tasks.values())
+		for(model.Task ta:tasks.values()) // Übergibt Tasks
 		{
 			xml.projects.Project.TaskEntries xmlTask = ocFachPro.createProjectTaskEntries();
 			xmlTask.setColor(ta.getFarbe());
@@ -79,14 +91,13 @@ public class XML_translator
 		Statusliste orginalStatusListe = orginalProjekt.getStatusliste();
 		
 		
-		//Statuslist sListe = ocFachPro.createProjectStatuslist();
 		
-		for(Status st:orginalStatusListe.getAll())
+		for(Status st:orginalStatusListe.getAll()) // übergibt StatusList
 		{
 			StatusEntries stat = ocFachPro.createProjectStatusEntries();
 			stat.setStatus(st.getName());
 			xmlProject.getStatusEntries().add(stat);
-			//sListe.getStatus().add(st.getName());
+			
 		}
 
 		prooverentries.setCreatedOn(orginalProjekt.getErstellungsDatum().toString());
@@ -95,37 +106,30 @@ public class XML_translator
 		xmlProject.setID(orginalProjekt.getId());
 		xmlProject.setLastMod(orginalProjekt.getLetzteAenderung().toString());
 		xmlProject.setProjectname(orginalProjekt.getProjektname());
-		//xmlProject.setStatuslist(sListe);
-		//xmlProject.setTasklist(tList)
+	
 		prooverentries.setID(orginalProjekt.getId());
 		
-		Xml_Server.addtoprojectList(xmlProject, prooverentries);
+		Xml_Server.saveProject(xmlProject, prooverentries); // speichert das Projekt
 
 	}
 
-	//**not in use
-	public void fügeTaskhinzu(Projekt orginalProjekt,model.Task orginalTask) throws JAXBException
-	{
-		ObjectFactoryProjects ocFachPro = new ObjectFactoryProjects();
-		xml.projects.Project.Tasklist.Task xmlTask = ocFachPro.createProjectTaskEntries();
-		
-		xmlTask.setColor(orginalTask.getFarbe());
-		xmlTask.setComment(orginalTask.getKommentar());
-		xmlTask.setID(orginalTask.getId());
-		xmlTask.setLastmod(orginalTask.getLetzteAenderung().toString());
-		xmlTask.setStatusname(orginalTask.getStatus().getName());
-		xmlTask.setTaskname(orginalTask.getName());
-		
-		Xml_Server.addTasktoProject(orginalProjekt.getProjektname(), xmlTask);
-	}
 	
 	
+	/**
+	 * Erhält einen User, gibt eine Hashmap wieder und übersetzt in das OrginalProjekt und gibt liste davon wieder
+	 * @param orginaluser
+	 * @return User der Projekt
+	 * @throws JAXBException
+	 * @throws DatatypeConfigurationException
+	 */
 	public List<Projekt> multiProject(User orginaluser) throws JAXBException, DatatypeConfigurationException
 	{
 		String userName = orginaluser.getNutzername();
-		HashMap<Project, ProjectOverviewEntries> pList = Xml_Server.checkProjectListandgiveProjectsback(userName);
+		HashMap<Project, ProjectOverviewEntries> pList = Xml_Server.getProjectsByUser(userName);  //erhält Hashmap
+		List<Projekt> orginalList = new ArrayList<Projekt>();
 		
-		for (Entry<Project, ProjectOverviewEntries> entry :pList.entrySet()) 
+		
+		for (Entry<Project, ProjectOverviewEntries> entry :pList.entrySet()) // geht durch die Hashmap
 		{
 			xml.projects.Project pro = entry.getKey();
 			ProjectOverviewEntries Over = entry.getValue();
@@ -141,7 +145,7 @@ public class XML_translator
 			
 			Statusliste slist = new Statusliste();
 			
-			for(StatusEntries stat: pro.getStatusEntries())
+			for(StatusEntries stat: pro.getStatusEntries()) // übergibt Statuslist
 			{
 				Status orgistat = new Status(stat.getStatus());
 				slist.insertStatus(orgistat);
@@ -149,7 +153,7 @@ public class XML_translator
 			
 			orginalProjekt.setStatusliste(slist);
 			
-			for( TaskEntries tAs : pro.getTaskEntries())
+			for( TaskEntries tAs : pro.getTaskEntries())  // geht durch Tasklist übersetzt
 			{
 				ZonedDateTime lasttime = ZonedDateTime.parse(tAs.getLastMod());
 				Task orgiTask = new Task(tAs.getTaskname(), null, null);
@@ -164,27 +168,39 @@ public class XML_translator
 				
 			}
 			
-			for(UserEntries usa:Over.getUserEntries())
+			for(UserEntries usa:Over.getUserEntries()) // übersetzt User
 			{
 				User orUser = new User(usa.getValue(), usa.isAdmin(), null, null);
 				orginalProjekt.addUserToHashMap(orUser);
 			}
+			orginalList.add(orginalProjekt);
+			
 	
 		}
+		return orginalList;
 
 	}
-	public Project singelProject(Integer ID) throws JAXBException, DatatypeConfigurationException
+	/**
+	 * gibt ID an XML Server, erhält Hashmap und übersetzt und gibt OrginalProjekt wieder
+	 * @param ID der Projekts
+	 * @return OrginalProjekt
+	 * @throws JAXBException
+	 * @throws DatatypeConfigurationException
+	 */
+	public Projekt singelProject(int ID) throws JAXBException, DatatypeConfigurationException
 	{
 		
-		
-		HashMap<Project, ProjectOverviewEntries> pList = Xml_Server.checkProjectListandgiveProjectsbackSingel(ID);
+		Projekt orginalProjekt = null;
+		HashMap<Project, ProjectOverviewEntries> pList = Xml_Server.getProjectByID(ID); // erhält Hashmap
 		
 		for (Entry<Project, ProjectOverviewEntries> entry :pList.entrySet()) 
 		{
+			if(entry.getKey()!= null) // wenn entry null dann geht er aus der Schleife , da nur ein Eintrag
+			{
 			xml.projects.Project pro = entry.getKey();
 			ProjectOverviewEntries Over = entry.getValue();
-			
-			Projekt orginalProjekt = new Projekt(Over.getCreator(),pro.getProjectname(),pro.getDescription());
+			User orgi = new User(Over.getCreator(), false, null, null);
+			orginalProjekt = new Projekt(orgi,pro.getProjectname(),pro.getDescription());
 			User orgiUser = new User(Over.getCreator(), true, null, null);
 			orginalProjekt.setErsteller(orgiUser);
 			orginalProjekt.setId(pro.getID());
@@ -195,7 +211,7 @@ public class XML_translator
 			
 			Statusliste slist = new Statusliste();
 			
-			for(StatusEntries stat: pro.getStatusEntries())
+			for(StatusEntries stat: pro.getStatusEntries()) // übersetzt die Status
 			{
 				Status orgistat = new Status(stat.getStatus());
 				slist.insertStatus(orgistat);
@@ -203,7 +219,7 @@ public class XML_translator
 			
 			orginalProjekt.setStatusliste(slist);
 			
-			for( TaskEntries tAs : pro.getTaskEntries())
+			for( TaskEntries tAs : pro.getTaskEntries()) // übersetzt die Tasks
 			{
 				ZonedDateTime lasttime = ZonedDateTime.parse(tAs.getLastMod());
 				Task orgiTask = new Task(tAs.getTaskname(), null, null);
@@ -218,13 +234,17 @@ public class XML_translator
 				
 			}
 			
-			for(UserEntries usa:Over.getUserEntries())
+			for(UserEntries usa:Over.getUserEntries())  // übersetzt die USer
 			{
 				User orUser = new User(usa.getValue(), usa.isAdmin(), null, null);
 				orginalProjekt.addUserToHashMap(orUser);
 			}
+			
+			}
+			
 	
 		}
+		return orginalProjekt;
 	}
 	
 
